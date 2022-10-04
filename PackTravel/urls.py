f49226c71301405c301fada38608a9bbb7c68cc1
amisha-w <amishabw@gmail.com>
2022-10-04 16:40:03 -0400

    Adding home view

--- PackTravel/urls.py
+++ PackTravel/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from user import views as userView
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', userView.index, name ='index'),
 ]
