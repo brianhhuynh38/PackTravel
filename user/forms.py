55c0b4a0698b312d6d2ae84cf03655ec5e137994
Aoishi Das <aoishidas28@gmail.com>
2022-10-04 17:32:43 -0400

    Adding registration form

---
+++ user/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField()
+    email = forms.EmailField(required=True, max_length=60)
+    phone_number = forms.CharField(required=True, max_length=11)
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+            'phone_number',
+        )
--- user/views.py
+++ user/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import requests
 import json
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Home page for PackTravel
 def index(request):
     return render(request, 'home/home.html')
+
+
+def home(request):
+    return render(request, 'home/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created!! You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'user/register.html', context)
