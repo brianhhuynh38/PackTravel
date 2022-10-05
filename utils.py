32fca8941a62238ca077d5357d42e106a45274d0
Aoishi Das <aoishidas28@gmail.com>
2022-10-04 20:33:11 -0400

    Adding MongoClient

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ def get_client():
     with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
         content=f.readlines()
         
-    print("\n\n=========================================================================\n",content)
     client = MongoClient("mongodb+srv://Aoishi:"+content[0]+"@cluster0.zpuftvw.mongodb.net/?retryWrites=true&w=majority")
 
     
