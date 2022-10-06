1233954b21caf9c056521fbc73c347a2f7c61f91
Swarnamalya M <swarnamalya65@gmail.com>
2022-10-06 19:05:39 -0400

    Creating rides backend

--- publish/models.py
+++ publish/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# declare a new model with a name "GeeksModel"
+class Ride(models.Model):
+    # fields of the model
+    destination = models.TextField()
+    rideDate = models.TextField()
+
+    class Meta:
+        app_label = 'PackTravel.publish'
+
+    # renames the instances of the model
+    # with their title name
+    def __str__(self):
+        return self.title
