f13f2e20268bf5483f1e8adfcf62b6131fb299e3
Kunal Shah <kunaljshah03@gmail.com>
2022-10-07 10:54:54 -0400

    code formatting

--- user/link.py
+++ user/link.py
@@ -33,7 +33,7 @@ def get_link(source,destination,date_tr,time_tr,f):
         diff=int(date_tr)-int(date[-1])
         for i in range(diff):
             browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[2]/span[2]/button[2]').click()
-    
+
     time_n=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[1]/input')
     time_n.send_keys(Keys.CONTROL + "a")
     time_n.send_keys(Keys.DELETE)
@@ -41,18 +41,18 @@ def get_link(source,destination,date_tr,time_tr,f):
     time.sleep(2)
 
     if(f==2):
-	    browser.find_element("xpath",'//*[@id="omnibox-directions"]/div/div[2]/div/div/div/div[3]/button').click()
-            time.sleep(2)
+        browser.find_element("xpath",'//*[@id="omnibox-directions"]/div/div[2]/div/div/div/div[3]/button').click()
+        time.sleep(2)
 
     browser.find_element("xpath",'//*[@id="section-directions-trip-title-0"]').click()
     time.sleep(2)
 
     if(f==2):
-	    route=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[2]').text
-            info=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[3]/div').text
-	    route=route+" , "+info
+        route=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[2]').text
+        info=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[3]/div').text
+        route=route+" , "+info
     else:
-	    route="Cab"
+        route="Cab"
 
     share=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/button')
     share.click()
