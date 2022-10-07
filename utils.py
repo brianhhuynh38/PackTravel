15ee1c2385d40ab7d776f39a7d244f239d3aba4f
Kunal Shah <kunaljshah03@gmail.com>
2022-10-07 10:10:01 -0400

    Formatting files

--- utils.py
+++ utils.py
@@ -5,7 +5,6 @@ import sys
 def get_client():
     with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
         content=f.readlines()
-        
     client = MongoClient("mongodb+srv://Aoishi:"+content[0]+"@cluster0.zpuftvw.mongodb.net/?retryWrites=true&w=majority")
 
     
