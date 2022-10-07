579d0c27238c0dc26be582028d27635b23000813
Aoishi Das <aoishidas28@gmail.com>
2022-10-07 11:09:57 -0400

    Updating the links file

--- user/link.py
+++ user/link.py
@@ -4,61 +4,78 @@ from selenium.webdriver.common.keys import Keys
 import chromedriver_autoinstaller
 
 # f : Cab or Bus (1= Cab, 2=Bus)
-def get_link(source,destination,date_tr,time_tr,f):
+def get_link(source,destination,date_tr,month_tr,time_tr,f):
 
-    chromedriver_autoinstaller.install()
-    chrome_options = webdriver.ChromeOptions()
-    browser = webdriver.Chrome(options=chrome_options)
-    browser.get('https://www.google.com/maps')
-    box=browser.find_element("xpath",'//*[@id="searchboxinput"]')
-    box.click()
+	chromedriver_autoinstaller.install()
+	chrome_options = webdriver.ChromeOptions()
+	browser = webdriver.Chrome(options=chrome_options)
 
-    box.send_keys(destination) 
-    box.send_keys(Keys.ENTER)
-    time.sleep(4)
+	dict_month={'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+	month_tr=dict_month[month_tr]
+	date_tr=int(date_tr)
+	browser.get('https://www.google.com/maps')
+	box=browser.find_element("xpath",'//*[@id="searchboxinput"]')
+	box.click()
 
-    dir=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button')
-    dir.click()
-    time.sleep(4)
-    box=browser.find_element("xpath",'//*[@id="sb_ifc51"]/input')
-    box.click()
-    box.send_keys(source) 
-    box.send_keys(Keys.ENTER)
-    time.sleep(2)
+	box.send_keys(destination) 
+	box.send_keys(Keys.ENTER)
+	time.sleep(4)
 
-    browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/span/div/div/div/div[2]').click()
-    browser.find_element("xpath",'//*[@id=":9"]/div').click()
-    date=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[2]/span[1]').text
-    if(int(date[-1])!=int(date_tr)):
-        diff=int(date_tr)-int(date[-1])
-        for i in range(diff):
-            browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[2]/span[2]/button[2]').click()
-    
-    time_n=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[1]/input')
-    time_n.send_keys(Keys.CONTROL + "a")
-    time_n.send_keys(Keys.DELETE)
-    time_n.send_keys(time_tr)
-    time.sleep(2)
+	dir=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button')
+	dir.click()
+	time.sleep(4)
+	box=browser.find_element("xpath",'//*[@id="sb_ifc51"]/input')
+	box.click()
+	box.send_keys(source) 
+	box.send_keys(Keys.ENTER)
+	time.sleep(2)
 
-    if(f==2):
-	    browser.find_element("xpath",'//*[@id="omnibox-directions"]/div/div[2]/div/div/div/div[3]/button').click()
-            time.sleep(2)
+	browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/span/div/div/div/div[2]').click()
+	browser.find_element("xpath",'//*[@id=":9"]/div').click()
+	date_today=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[2]/span[1]').text
+	
+	month=dict_month[date_today[-5:-2]]
+	date=int(date_today[-1])
+	if(date_tr!=date):
+		if(month == month_tr):
+			diff=date_tr-date
+		else:
+			if(month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12):
+				diff=date_tr+31-date
+			elif(month==2):
+				diff=date_tr+28-date
+			else:
+				diff=date_tr+30-date
 
-    browser.find_element("xpath",'//*[@id="section-directions-trip-title-0"]').click()
-    time.sleep(2)
+		for i in range(diff):
+			browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[2]/span[2]/button[2]').click()
+			time.sleep(2)
+			
+	time_n=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/span[1]/input')
+	time_n.send_keys(Keys.CONTROL + "a")
+	time_n.send_keys(Keys.DELETE)
+	time_n.send_keys(time_tr)
+	time.sleep(2)
 
-    if(f==2):
-	    route=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[2]').text
-            info=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[3]/div').text
-	    route=route+" , "+info
-    else:
-	    route="Cab"
+	if(f==2):
+		browser.find_element("xpath",'//*[@id="omnibox-directions"]/div/div[2]/div/div/div/div[3]/button').click()
+		time.sleep(2)
 
-    share=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/button')
-    share.click()
-    time.sleep(2)
-    emb=browser.find_element("xpath",'//*[@id="modal-dialog"]/div/div[2]/div/div[3]/div/div/div[1]/div[2]/button[2]')
-    emb.click()
-    time.sleep(4)
-    link=browser.find_element("xpath",'//*[@id="modal-dialog"]/div/div[2]/div/div[3]/div/div/div/div[3]/div[1]/input').get_attribute("value")
-    return(route,link)
+	browser.find_element("xpath",'//*[@id="section-directions-trip-title-0"]').click()
+	time.sleep(2)
+
+	if(f==2):
+		route=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[2]').text
+		info=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div[2]/div/div[3]/div').text
+		route=route+" , "+info
+	else:
+		route="Cab"
+
+	share=browser.find_element("xpath",'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]/div[2]/div[2]/button')
+	share.click()
+	time.sleep(2)
+	emb=browser.find_element("xpath",'//*[@id="modal-dialog"]/div/div[2]/div/div[3]/div/div/div[1]/div[2]/button[2]')
+	emb.click()
+	time.sleep(4)
+	link=browser.find_element("xpath",'//*[@id="modal-dialog"]/div/div[2]/div/div[3]/div/div/div/div[3]/div[1]/input').get_attribute("value")
+	return(route,link)
