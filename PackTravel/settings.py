02340c5fe008069215da1d8fe0a13f415eedf4a5
sankettangade <sankettangade99@gmail.com>
2022-12-05 14:02:47 -0500

    Fixed search ride page

--- PackTravel/settings.py
+++ PackTravel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lncd0d7jfejnf7_2ycdtawwo&$_y=q$n8#%2fk5bxaqy$*9mwo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.83.178.112']
+ALLOWED_HOSTS = ['3.83.178.112', '127.0.0.1']
 
 
 # Application definition
