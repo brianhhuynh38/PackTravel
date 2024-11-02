130e45028c746e277aa0625feeb8f4ba134db9ea
MakarandPundlik <mmp2308@gmail.com>
2024-11-01 20:08:03 -0400

    removed hardcoded password from utils.py

--- utils.py
+++ utils.py
@@ -30,12 +30,10 @@ from pymongo import MongoClient
 
 def get_client():
     """Returns a MongoDB client connected to the specified Atlas cluster with secure TLS authentication."""
-    #with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
-        #content = f.readlines()
-    content="Varun1406"    
+    with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
+        content = f.readlines()
+        #file - config.ini stores password 
     client = MongoClient(
-        "mongodb+srv://vvarath:" + content + "@cluster0.wc05z.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb+srv://<username>:"+content[0]+"@cluster0.wc05z.mongodb.net/?retryWrites=true&w=majority",
         tlsCAFile=certifi.where())
-        #"mongodb+srv://Aoishi:" + content[0] + "@cluster0.zpuftvw.mongodb.net/?retryWrites=true&w=majority",
-
     return client
